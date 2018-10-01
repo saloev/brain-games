feb4a8604bef96f5bcb864c90f1d40ff72c38c3c
saloev <saloev.saadi@yandex.ru>
2018-10-01 21:09:58 +0300

    add greating lib and write greeting func

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
-console.log("Welcome to the Brain Games!");
+import greetingUser from '..';
+console.log(`Welcome to the Brain Games!`);
+console.log(`Hello, ${greetingUser()}`);
 
