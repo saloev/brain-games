7772c0346437cfa0d2b705b18c994c13c621d2e0
saloev <saloev.saadi@yandex.ru>
2018-10-03 14:01:40 +0300

    change even-game.js for succesfull linting

--- src/games/calc-game.js
+++
@@ -1,1 +0,0 @@
-import readlineSync from 'readline-sync';
--- src/games/even-game.js
+++ src/games/even-game.js
@@ -5,7 +5,6 @@ const countOfCorrectAnswers = 3;
 const isEven = num => num % 2 === 0;
 
 const evenGame = (player) => {
-  
   for (let counter = 1; counter <= countOfCorrectAnswers; counter += 1) {
     const question = Math.ceil(Math.random() * 50);
 
