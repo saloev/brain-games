69b989c03a3cbb31394dd9d457fc9d5eccea3a9f
saloev <saloev.saadi@yandex.ru>
2018-10-03 13:36:11 +0300

    chnage even-games.js

--- src/games/even-game.js
+++ src/games/even-game.js
@@ -5,11 +5,7 @@ const countOfCorrectAnswers = 3;
 const isEven = num => num % 2 === 0;
 
 const evenGame = (player) => {
-  let counter = 0;
-
-  while (counter < countOfCorrectAnswers) {
-    counter += 1;
-
+  for (let counter = 1; counter <= countOfCorrectAnswers; counter += 1) {
     const question = Math.ceil(Math.random() * 50);
 
     console.log(`Question: ${question}`);
